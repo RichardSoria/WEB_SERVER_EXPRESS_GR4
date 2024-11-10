f406e4c0f5a952661343c220aece10e27029b0ab
Richard Mauricio Soria Asazan <richard.soria@epn.edu.ec>
2024-11-09 23:49:46 -0500

    Changes DataBase

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import express from 'express'
+import { create, router as jsonRouter } from 'json-server'
+import path from 'path'
 import router from './routers/member_routes.js'
 
 // Inicialización
 const app = express()
+const jsonServer = create()
+const dbRouter = jsonRouter(path.join(__dirname, '../db.json'))
 
 // Varaibles
 app.set('port', process.env.PORT || 3000)
 
 // Middlewares
 app.use(express.json())
+
+// JSON Server Routes
+app.use('/api',dbRouter)
 
 // Rutas
 app.get('/',(req,res)=>{
