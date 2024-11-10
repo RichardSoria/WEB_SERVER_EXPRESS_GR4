e30f6a3830c351bbf090bcea7b477901ec82c026
Richard Mauricio Soria Asazan <richard.soria@epn.edu.ec>
2024-11-09 23:34:06 -0500

    Changes vercel

--- src/models/member.js
+++ src/models/member.js
@@ -1,26 +1,24 @@
-const API_URL = process.env.API_URL || 'http://localhost:4000'
+const API_URL = process.env.API_URL || 'http://localhost:4000';
 
 const memberModel = {
-    async getAllMembersModel(){
-        try{
-            const peticion = await fetch(`${API_URL}/members`)
-            const members = await peticion.json()
-            return members
-        }
-        catch (error){
-            console.log(error)
+    async getAllMembersModel() {
+        try {
+            const peticion = await fetch(`${API_URL}/members`);
+            const members = await peticion.json();
+            return members;
+        } catch (error) {
+            console.log(error);
         }
     },
-    async getMemberByIdModel(id){
-        try{
-            const peticion = await fetch(`${API_URL}/members/${id}`)
-            const member = await peticion.json()
-            return member
-        }
-        catch (error){
-            console.log(error)
+    async getMemberByIdModel(id) {
+        try {
+            const peticion = await fetch(`${API_URL}/members/${id}`);
+            const member = await peticion.json();
+            return member;
+        } catch (error) {
+            console.log(error);
         }
     }
 }
 
-export default memberModel
+export default memberModel;
